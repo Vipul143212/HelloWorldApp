fe8c1717e8ff25c73c21637f4220ebf671e5a9e0
Vipul <vipulupadhyay563@gmail.com>
2023-08-07 14:36:02 +0530

    Add project files.

---
+++ HelloWorldApp/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace HelloWorldApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            double Input_1 = 0, Input_2 = 0;
+            checked
+            {
+                try
+                {
+                    do
+                    {
+                        Console.Write("Please Enter First Number : ");
+
+                    } while (!double.TryParse(Console.ReadLine(), out Input_1));
+                    do
+                    {
+                        Console.Write("Please Enter Second Number : ");
+
+                    } while (!double.TryParse(Console.ReadLine(), out Input_2));
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+        private static double Sum(double x, double y)
+        {
+            return x + y;
+        }
+    }
+}
